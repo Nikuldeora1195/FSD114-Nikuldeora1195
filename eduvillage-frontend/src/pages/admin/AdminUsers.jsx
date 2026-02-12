f83417362160031e3d48e0016f0e31409e646281
nikuldeora1195 <nikuldeora1195@gmail.com>
2026-02-12 22:45:20 +0530

    Temp rename admin api

--- eduvillage-frontend/src/pages/admin/AdminUsers.jsx
+++ eduvillage-frontend/src/pages/admin/AdminUsers.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { getAllUsers, updateUserRole, updateUserStatus } from "../../api/adminApiTemp";
+import { getAllUsers, updateUserRole, updateUserStatus } from "../../api/adminApi";
 // import AdminSidebar from "../../components/admin/AdminSidebar";
 import toast from "react-hot-toast";
 
